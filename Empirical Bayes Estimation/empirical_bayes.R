6472d42fcf0e182cd0b99a6f54bd4912f2319cc0
vincentvangoat <menogetusername@gmail.com>
2016-12-30 13:29:23 -0600

    empirical bayesian shrinkage

--- Empirical Bayes Estimation/empirical_bayes.R
+++ Empirical Bayes Estimation/empirical_bayes.R
@@ -3,6 +3,8 @@ library(tidyr)
 library(Lahman)
 library(ggplot2)
 library(ggthemes)
+library(MASS)
+library(broom)
 
 #colors: 
 yellow <- "#f1c40f"
@@ -24,10 +26,6 @@ batters <- Master %>%
   unite(name, nameFirst, nameLast, sep = " ") %>%
   inner_join(batters, by = "playerID") %>%
   select(-playerID)
-  
-batters %>%
-  arrange(avg) %>%
-  head(5)
 
 batters %>%
   filter(AB >= 500) %>%
@@ -35,4 +33,38 @@ batters %>%
   geom_histogram(binwidth = .005, fill = yellow) + 
   theme_fivethirtyeight() + 
   ggtitle("Distribution of Batters' Batting Averages")
+
+batters_filtered <- batters %>%
+  filter(AB >= 500)
+
+#Fit a maximum likelihood model to the beta distribution
+mle <- fitdistr(batters_filtered$avg, "beta", start = list(shape1 = 1, shape2 = 12))
+tidy(mle)
+
+#get estimates for alpha and beta
+alpha = tidy(mle)$estimate[1]
+beta = tidy(mle)$estimate[2]
+
+batters_filtered %>%
+  ggplot() +
+  geom_histogram(aes(avg, y = ..density..),binwidth = .005, fill = yellow) + 
+  stat_function(fun = function(x) dbeta(x, alpha, beta), color = blue, size = 2) + 
+  theme_fivethirtyeight() + 
+  ggtitle("Fitting beta distribution with estimated priors")
+
+batters_estimates <- batters_filtered %>%
+  mutate(estimate = (alpha + Hits)/(alpha + beta + AB))
+
+#Empirical Bayesian shrinkage towards a beta prior xd
+
+ggplot(batters_estimates, aes(avg, estimate, color = AB)) +
+  geom_hline(yintercept = alpha / (alpha + beta), color = red, lty = 2) +
+  geom_point() +
+  geom_abline(color = red) +
+  scale_colour_gradient(trans = "log", breaks = 10 ^ (1:5)) +
+  xlab("Batting average") +
+  ylab("Empirical Bayes batting average")
+
+# These extraordinary outliers need extraordinary evidence
+
 
