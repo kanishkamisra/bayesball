5fc33c5265c59f9a8c4b607f70cb52a96bcaddf6
vincentvangoat <menogetusername@gmail.com>
2017-01-03 17:31:16 -0600

    Normal approximation of beta

--- AB testing/ab_testing.R
+++ AB testing/ab_testing.R
@@ -68,3 +68,50 @@ sim
 
 # There is 60.6% probability that piazza is better than hank aaron..
 
+# Density Cloud
+x <- seq(.29, .318, .0002)
+crossing(piazza_x = x, aaron_x = x) %>%
+  mutate(piazza_density = dbeta(piazza_x, mpiazza$alpha1, mpiazza$beta1),
+         aaron_density = dbeta(aaron_x, haaron$alpha1, haaron$beta1),
+         joint = piazza_density * aaron_density) %>%
+  ggplot(aes(piazza_x, aaron_x, fill = joint)) +
+  geom_tile() +
+  geom_abline() +
+  scale_fill_gradient2(low = "white", high = red) +
+  labs(x = "Piazza batting average",
+       y = "Aaron batting average",
+       fill = "Joint density") +
+  theme_fivethirtyeight()
+
+
+# Closed form approximation (Normal approximation of the beta function)
+
+two_players %>%
+  mutate(mu = alpha1 / (alpha1 + beta1),
+         var = alpha1 * beta1 / ((alpha1 + beta1) ^ 2 * (alpha1 + beta1 + 1))) %>%
+  inflate(x = seq(.28, .33, .00025)) %>%
+  mutate(density = dbeta(x, alpha1, beta1),
+         normal = dnorm(x, mu, sqrt(var))) %>%
+  ggplot(aes(x, density, group = name)) +
+  geom_line(aes(color = name), size = 1.75) +
+  geom_line(lty = 2, size = 1.5) + 
+  theme_fivethirtyeight()
+
+# Virtually indistinguishable lines!
+
+# Probability that one normal is greater than the other:
+h_approx <- function(alpha_a, beta_a,
+                     alpha_b, beta_b) {
+  u1 <- alpha_a / (alpha_a + beta_a)
+  u2 <- alpha_b / (alpha_b + beta_b)
+  var1 <- alpha_a * beta_a / ((alpha_a + beta_a) ^ 2 * (alpha_a + beta_a + 1))
+  var2 <- alpha_b * beta_b / ((alpha_b + beta_b) ^ 2 * (alpha_b + beta_b + 1))
+  pnorm(0, u2 - u1, sqrt(var1 + var2))
+}
+
+h_approx(mpiazza$alpha1, mpiazza$beta1, haaron$alpha1, haaron$beta1)
+# Again, 60.6% in favor of Mike Piazza!
+
+
+
+
