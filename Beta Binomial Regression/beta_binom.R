7501fb0ec805188830e6399887959bce19e9c2bf
vincentvangoat <menogetusername@gmail.com>
2017-01-04 18:13:27 -0600

    beta binomial regression started

--- Beta Binomial Regression/beta_binom.R
+++ Beta Binomial Regression/beta_binom.R
@@ -4,8 +4,11 @@ library(Lahman)
 library(ggplot2)
 library(ggthemes)
 library(broom)
+library(extrafont)
 
 options(digits = 3)
+font_import()
+loadfonts(device = "win")
 
 #colors: 
 yellow <- "#f1c40f"
@@ -43,3 +46,55 @@ batters_estimates <- batters_filtered %>%
   mutate(estimate = (alpha + Hits)/(alpha + beta + AB), alpha1 = (Hits + alpha),
          beta1 = (AB - Hits + beta)) %>%
   arrange(desc(estimate))
+
+# Add this to initial thing as well
+# How averages vary over at-bats.
+batters %>%
+  filter(AB >= 20) %>%
+  ggplot(aes(AB, avg)) +
+  geom_point(color = yellow) +
+  geom_smooth(method = "lm", se = FALSE, color = blue, size = 1.5) +
+  scale_x_log10() + 
+  theme_fivethirtyeight() + 
+  theme(plot.title=element_text(family="Roboto")) +
+  theme(axis.title = element_text(family = "Roboto", face = "bold", color="#666666", size = 12)) +
+  theme(axis.text = element_text(family = "Roboto", face = "bold", color = "#535353", size = 11)) +
+  geom_hline(yintercept=0,size=1.2,colour="#535353") +
+  ggtitle("At-bats vs logarithmic batting average")
+
+# Lower ABs have more variance, as AB increases, average also increases.
+# Better batters are played more so they tend to increase the average batting average
+# as number of games go up.
+
+batters_estimates %>%
+  filter(AB >= 20) %>%
+  gather(type, value, avg, estimate) %>%
+  mutate(type = plyr::revalue(type, c(avg = "Raw",
+                                      estimate = "With EB Shrinkage"))) %>%
+  ggplot(aes(AB, value)) +
+  geom_point(color = yellow) +
+  geom_hline(color = red, size = 1.5, yintercept = prior_mu) +
+  facet_wrap(~type) +
+  ylab("average") +
+  geom_smooth(method = "lm", color = blue, size = 1.5) +
+  scale_x_log10() +
+  theme_fivethirtyeight() + 
+  theme(plot.title=element_text(family="Roboto")) +
+  theme(axis.title = element_text(family = "Roboto", face = "bold", color="#666666", size = 12)) +
+  theme(axis.text = element_text(family = "Roboto", face = "bold", color = "#535353", size = 11)) +
+  theme(strip.text.x = element_text(family = "Roboto", face = "bold", size = 11)) + 
+  ggtitle("At-bats vs log averages and estimates") +
+  # geom_hline(yintercept=0,size=1.2,colour="#535353") +
+  labs(subtitle = "Kanishka Misra")
+
+# Low AB value players are getting grossly overestimated by the shrinkage model
+# There is a need to make a model that considers AB
+
+# By re-parameterizing the true probability(posterior) distribution, we can express it in terms of
+# mean of the batting average and how spread out the distribution is
+# p ~ Beta(mu/sigma, (1-mu)/sigma)
+
+# To include AB, we can define mu as 
+# I give up
+
+
